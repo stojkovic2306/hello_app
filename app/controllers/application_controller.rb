d6add71fe91870fed93bf9be6470f6ca4fd26efd
Marko Stojkovic <59100869+stojkovic2306@users.noreply.github.com>
2026-03-01 21:08:45 +0100

    Added a Hello World controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+
+  def hello
+    render html: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render html: "Goodbye, World!"
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  # Allow connections to local server on cloud IDE.
+  config.hosts.clear
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
